c4707a6c512603514c30abb14e1769c40c718734
letsjo <hyunoh.jo@gmail.com>
2023-02-03 23:02:38 +0900

    feat(index): modeling createTodo

--- src/index.js
+++ src/index.js
@@ -13,3 +13,20 @@
  * @property {string} category - todo category (reqired)
  * @property {Tag[]} tags - tags string array (reqired)
  */
+
+
+
+/**
+ * A createTodo function params type.
+ * @typedef {Object} CreateTodo - CreateTodo type.
+ * @property {string} content - todo content (reqired)
+ * @property {string} category - todo category (reqired)
+ * @property {Array<string>=} tags - tags string array (optional)
+ */
+
+/**
+ * A createTodo function create todo.
+ * @param {CreateTodo} props content(reqired), category(reqired), tags(optional)
+ * @returns {Promise<Todo>} return todo value.
+ */
+async function createTodo({ content, category, tags }) {}
