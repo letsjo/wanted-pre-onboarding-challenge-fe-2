a3adf5c2e08adf96c7a348e60b913e0761b8a63d
letsjo <hyunoh.jo@gmail.com>
2023-02-03 23:01:46 +0900

    feat(index): modeling todo data type

--- src/index.js
+++ src/index.js
@@ -1,51 +1,15 @@
 /**
- * Represents a book.
- * @constructor
- * @param {string} title - The title of the book.
- * @param {string} author - The author of the book.
+ * @typedef {Object} Tag - tag type.
+ * @property {string} id - tag id (reqired)
+ * @property {string} name - tag name (reqired)
  */
-function Book(title, author) {}
 
 /**
- * Both of these will link to the bar function.
- * @see {@link bar}
- * @see bar
+ * A Todo data type
+ * @typedef {Object} Todo - Todo type
+ * @property {string} id - todo id (reqired)
+ * @property {string} content - todo content (reqired)
+ * @property {boolean} completed - todo status (reqired)
+ * @property {string} category - todo category (reqired)
+ * @property {Tag[]} tags - tags string array (reqired)
  */
-function foo() {}
-
-// Use the inline {@link} tag to include a link within a free-form description.
-/**
- * @see {@link foo} for further information.
- * @see {@link http://github.com|GitHub}
- */
-function bar() {}
-
-/**
- * Generic dairy product.
- * @constructor
- */
-function DairyProduct() {}
-
-/**
- * Check whether the dairy product is solid at room temperature.
- * @abstract
- * @return {boolean}
- */
-DairyProduct.prototype.isSolid = function () {
-	throw new Error('must be implemented by subclass!');
-};
-
-/**
- * Cool, refreshing milk.
- * @constructor
- * @augments DairyProduct
- */
-function Milk() {}
-
-/**
- * Check whether milk is solid at room temperature.
- * @return {boolean} Always returns false.
- */
-Milk.prototype.isSolid = function () {
-	return false;
-};
